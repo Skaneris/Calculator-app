87d3e38e9094ee4d207ed1ddf44ff8cfe82a301f
Sergey Prijan <dneprserp355@gmail.com>
2021-11-10 04:32:24 +0200

    initial dist

---
+++ precache-manifest.432448b730e9be8162fb847ddad3a22e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8313a29038bc0afd65",
+    "url": "/css/app.497bf1eb.css"
+  },
+  {
+    "revision": "754060bfe0b2c4ffe1233d28c3ebf90a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b8313a29038bc0afd65",
+    "url": "/js/app.ead6390e.js"
+  },
+  {
+    "revision": "092f4c76f333e582e83f",
+    "url": "/js/chunk-vendors.48bf29ca.js"
+  },
+  {
+    "revision": "3aab309b20216cd76aab23a57c10925c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import store from './store'
-import './assets/app.sass'
-
-createApp(App).use(store).mount('#calc_app')
